7e0357c23be9c3a0a4f48543f525f5a9c0260737
r44 <r790822@gmail.com>
2014-06-09 17:20:57 +0800

    Set up struct for analysis codes, leaving for dinner~

--- analysis/DataFrames.py
+++ analysis/DataFrames.py
@@ -5,6 +5,9 @@ from itertools import groupby
 import pprint   # pretty print for list
 import operator
 
+import numpy as np
+import scipy as sp
+
 class DataFrames:
     def __init__(self,filename):
 ## Parse the raw csv file to get the range of each parameter and the data as list of tuples.
@@ -31,13 +34,15 @@ class DataFrames:
                 rowdict['WChoice'] = Wei;
                 rowdict['RepeatTime'] = Timeidx;
                 rowdict['Pivots'] = Pivots;
+                self.NumLevel = len(Pivots)
                 rowdict['LevelRs'] = LevelRs;
                 rowdict['Cost'] = int(rowdict['Cost'])
                 rowdict['QCost'] = int(rowdict['QCost'])
                 rowdict['MatCost'] = int(rowdict['MatCost'])
+                self.MatCost = rowdict['MatCost']
                 rowdict['NaiveCost'] = int(rowdict['NaiveCost'])
-
                 data.append(rowdict)
+
         self.KList = sorted(KDict.keys())
         self.QidList = sorted(QidDict.keys())
         self.NumForEachList = sorted(NumForEachDict.keys())
@@ -48,10 +53,23 @@ class DataFrames:
 
     def SimpStat(self):
         #pprint.pprint( self.data)
-        print self.data
-        print 'g'
-        print self.data.sort(key=operator.itemgetter(('k','qid')));
-        print self.data
-        results = [];
-        sortkeyfn = key=lambda s:s[0:6]
-#        for key,
+
+
+        sortkeyfn = operator.itemgetter('k','NumForEach','NumMachine','WChoice');
+        self.data.sort(key=sortkeyfn);  # Need sort before groupby.
+
+        results = collections.defaultdict(dict);
+        for key, group in groupby(self.data, key = sortkeyfn):
+            Cost = []; NaiveCost = []; RsList = list();
+            for row in group:
+                Cost.append(row['Cost'])
+                NaiveCost.append(row['NaiveCost'])
+                tmp_arr = np.zeros(self.NumLevel)
+                for i in xrange(len(row['LevelRs'])):
+                    tmp_arr[i] = row['LevelRs'][i]
+                RsList.append(tmp_arr)
+            results[key] = {'Cost':np.array(Cost), 'NaiveCost':np.array(NaiveCost), 'LevelRs':np.array(RsList)}
+
+        for key,value in results.iteritems():
+            print value['LevelRs'].shape
+        print self.NumLevel
