c1ee7559a83c2ee7e4b182c3f9103587f3ee47b1
r44 <r790822@gmail.com>
2014-06-14 18:13:10 +0800

    Running the Exp0614

---
+++ batch_exp/New/backup/CheckData.py
@@ -0,0 +1,163 @@
+import sys
+import scipy.io as spio
+#from random import randrange
+from random import sample
+from random import seed
+sys.path.append('..')
+from MsWave import MsWave
+from Site import Site
+from Arff2Mat import Arff2Mat
+from numpy import *
+from math import log
+import csv
+
+def gen_pivot(data):
+    T = data.shape[1]
+    # Origin
+    pivot = [int(T/2+T/32*i) for i in range(40) if T/2+T/32*i <= T]
+    pivot[-1] = T
+    return pivot
+
+#main
+#################################################################################
+
+# Paramaters which wouldn't be changed.
+
+#data = (spio.loadmat('../LabelMe'))['data']
+HomePath = '/nfs/master/01/r01922165/zzzzz/'
+FlickDir = 'Dataset/Image/Flickr/flickr/ParsedData/'
+ArffDir = 'Dataset/Sound/0611/'
+#WeightPath = HomePath+'trans_ANN/Weights/'
+OutPath = HomePath+'Results/Exp0605/Flickr/500_2000/New/'
+FeaTypeList = range(7,11);
+for FeaType in FeaTypeList:
+    if FeaType == 1:
+        data = (spio.loadmat(HomePath+FlickDir+'1_ColorLayout192'))['data']
+    elif FeaType == 2:
+        data = (spio.loadmat(HomePath+FlickDir+'2_ColorStruct256'))['data']
+    elif FeaType == 3:
+        data = (spio.loadmat(HomePath+FlickDir+'3_ScalColor256'))['data']
+    elif FeaType == 4:
+        data = (spio.loadmat(HomePath+FlickDir+'4_HomoText43'))['data']
+    elif FeaType == 5:
+        data = (spio.loadmat(HomePath+FlickDir+'5_EdgeHist150'))['data']
+    elif FeaType == 0:
+        data = (spio.loadmat(HomePath+'ANNsift_base'))['data'].T
+    elif FeaType == 6:
+        data = Arff2Mat(HomePath+ArffDir+'msd-mvd-v1.0.arff/msd-mvd.arff')
+    elif FeaType == 7:
+        data = Arff2Mat(HomePath+ArffDir+'msd-rh-v1.0.arff/msd-rh.arff')
+    elif FeaType == 8:
+        data = Arff2Mat(HomePath+ArffDir+'msd-rp-v1.0.arff/msd-rp.arff')
+    elif FeaType == 9:
+        data = Arff2Mat(HomePath+ArffDir+'msd-ssd-v1.0.arff/msd-ssd.arff')
+    elif FeaType == 10:
+        data = Arff2Mat(HomePath+ArffDir+'msd-trh-v1.0.arff/msd-trh.arff')
+
+    print data.shape
+#    print mean(data,axis=0)
+#    print std(data,axis=0)
+#    print std(data,axis=0)/mean(data,axis=0)
+#    print mean(std(data,axis=0)/mean(data,axis=0))
+    print sum((std(data,axis=0)/mean(data,axis=0))**2)/data.shape[1]
+
+exit()
+
+OutFile = OutPath+str(FeaType)+'_0605.csv'
+fout = open(OutFile,'wb')
+headers = 'qid WChoice NHumMachine NumForEach k LevelRs Pivots RepeatTime MatCost NaiveCost Cost QCost'.split()
+dw = csv.DictWriter(fout,headers,restval='NULL');
+dw.writeheader()
+fout.close()
+
+seed(302)
+MaxNumMach = 2000;
+MaxNumForEach = 500;
+WeightPath = HomePath+'trans_flickr/'+str(FeaType)+'/'+str(MaxNumForEach)+'_'+str(MaxNumMach)+'/'
+FeaLen = data.shape[1]
+Total = data.shape[0]
+RepeatTime = 2;
+QList = sample(xrange(Total), RepeatTime)
+
+#################################################################################
+# Paramaters to be tuned.
+
+# Para for Exp 0605.
+"""
+kList = [1,5,10,15,20]
+NumForEachList = [100,200,300,400,500]
+NumMachList = [100,500,1000,1500,2000]
+WChoiceList = [1,2]
+"""
+
+# Para for testing.
+kList = [1,2]
+NumMachList = [20,200]
+NumForEachList = [500]
+WChoiceList = [1,2]
+#################################################################################
+record = dict()
+record['MatCost'] = FeaLen * (FeaLen-1) / 2;
+for WChoice in WChoiceList:
+    if WChoice == 1:
+        record['WChoice']='origin/';
+    elif WChoice == 2:
+        record['WChoice']='2_heur/';
+    else:
+        'Wchoice error'
+        exit()
+    # Initialize weight matrics.
+    WDict = dict();
+    for mid in range(MaxNumMach):
+        WDict[mid] = matrix( (spio.loadmat(WeightPath + record['WChoice'] +'X_'+str(MaxNumForEach)+'_'+str(mid+1)))['X'] ).T
+
+    for NumMach in NumMachList:
+        record['NumMachine']=NumMach;
+        # Gen pivot.
+        pivot = dict()
+        tmp_pivot = gen_pivot(data)
+        for mid in range(NumMach):
+            pivot[mid] = tmp_pivot
+        record['Pivots'] = '_'.join(str(x) for x in tmp_pivot)
+        del tmp_pivot
+
+        for NumForEach in NumForEachList:
+            record['NumForEach']=NumForEach
+            # Distribute data to each site
+            sites = dict()
+            for mid in range(NumMach):
+                s = mid*NumForEach;
+                e = s+NumForEach;
+                cand = dict()
+                for j in range(s,e):
+                    if j in QList:
+                        continue
+                    cand[j] = ((data[j]*WDict[mid]).tolist())[0]
+                sites[mid] = Site(mid, cand.keys(), cand)
+
+            for time in xrange(len(QList)):
+                qid = QList[time]
+                record['qid'] = qid
+                query = dict()
+                for mid in range(NumMach):
+                    query[mid] = matrix(data[qid])*WDict[mid]
+                for k in kList:
+                    record['k']=k
+                    ans, cost, level_rs, qcost =  MsWave(k, query, sites, pivot)
+                    naive = size(query[0])*NumMach + NumMach*k + k
+                    record['Cost'] = cost
+                    record['QCost'] = qcost
+                    record['NaiveCost']=naive
+                    record['LevelRs'] = '_'.join(str(x) for x in level_rs)
+                    record['RepeatTime']=time
+                    fout = open(OutFile,'ab')
+                    dw = csv.DictWriter(fout,headers,restval='NULL');
+                    dw.writerow(record)
+                    fout.close()
+                    for mid in range(NumMach):
+                        sites[mid].init_except_data();
+                print record
+                del query
+            del sites
+    del WDict
+fout.close()
---
+++ batch_exp/New/backup/run.py
@@ -0,0 +1,139 @@
+import sys
+import scipy.io as spio
+#from random import randrange
+from random import sample
+from random import seed
+sys.path.append('..')
+from MsWave import MsWave
+from Site import Site
+from numpy import *
+from math import log
+import csv
+
+def gen_pivot(data):
+    T = data.shape[1]
+    # Origin
+    pivot = [int(T/2+T/32*i) for i in range(40) if T/2+T/32*i <= T]
+    pivot[-1] = T
+    return pivot
+
+#main
+#################################################################################
+
+# Paramaters which wouldn't be changed.
+
+#data = (spio.loadmat('../LabelMe'))['data']
+#data = (spio.loadmat(HomePath+'ANNsift_base'))['data'].T
+HomePath = '/nfs/master/01/r01922165/zzzzz/'
+FlickDir = 'Dataset/Image/Flickr/flickr/ParsedData/'
+#WeightPath = HomePath+'trans_ANN/Weights/'
+OutPath = HomePath+'Results/Exp0605/Flickr/500_2000/New/'
+FeaType=1
+if FeaType == 1:
+    data = (spio.loadmat(HomePath+FlickDir+'1_ColorLayout192'))['data']
+elif FeaType == 2:
+    data = (spio.loadmat(HomePath+FlickDir+'2_ColorStruct256'))['data']
+elif FeaType == 3:
+    data = (spio.loadmat(HomePath+FlickDir+'3_ScalColor256'))['data']
+elif FeaType == 4:
+    data = (spio.loadmat(HomePath+FlickDir+'4_HomoText43'))['data']
+elif FeaType == 5:
+    data = (spio.loadmat(HomePath+FlickDir+'5_EdgeHist150'))['data']
+OutFile = OutPath+str(FeaType)+'_0605.csv'
+fout = open(OutFile,'wb')
+headers = 'qid WChoice NumMachine NumForEach k LevelRs Pivots RepeatTime MatCost NaiveCost Cost QCost'.split()
+dw = csv.DictWriter(fout,headers,restval='NULL');
+dw.writeheader()
+fout.close()
+
+seed(302)
+MaxNumMach = 2000;
+MaxNumForEach = 500;
+WeightPath = HomePath+'trans_flickr/'+str(FeaType)+'/'+str(MaxNumForEach)+'_'+str(MaxNumMach)+'/'
+FeaLen = data.shape[1]
+Total = data.shape[0]
+RepeatTime = 2;
+QList = sample(xrange(Total), RepeatTime)
+
+#################################################################################
+# Paramaters to be tuned.
+
+# Para for Exp 0605.
+"""
+kList = [1,5,10,15,20]
+NumForEachList = [100,200,300,400,500]
+NumMachList = [100,500,1000,1500,2000]
+WChoiceList = [1,2]
+"""
+
+# Para for testing.
+kList = [1,2]
+NumMachList = [20,200]
+NumForEachList = [500]
+WChoiceList = [1,2]
+#################################################################################
+record = dict()
+record['MatCost'] = FeaLen * (FeaLen-1) / 2;
+for WChoice in WChoiceList:
+    if WChoice == 1:
+        record['WChoice']='origin/';
+    elif WChoice == 2:
+        record['WChoice']='2_heur/';
+    else:
+        'Wchoice error'
+        exit()
+    # Initialize weight matrics.
+    WDict = dict();
+    for mid in range(MaxNumMach):
+        WDict[mid] = matrix( (spio.loadmat(WeightPath + record['WChoice'] +'X_'+str(MaxNumForEach)+'_'+str(mid+1)))['X'] ).T
+
+    for NumMach in NumMachList:
+        record['NumMachine']=NumMach;
+        # Gen pivot.
+        pivot = dict()
+        tmp_pivot = gen_pivot(data)
+        for mid in range(NumMach):
+            pivot[mid] = tmp_pivot
+        record['Pivots'] = '_'.join(str(x) for x in tmp_pivot)
+        del tmp_pivot
+
+        for NumForEach in NumForEachList:
+            record['NumForEach']=NumForEach
+            # Distribute data to each site
+            sites = dict()
+            for mid in range(NumMach):
+                s = mid*NumForEach;
+                e = s+NumForEach;
+                cand = dict()
+                for j in range(s,e):
+                    if j in QList:
+                        continue
+                    cand[j] = ((data[j]*WDict[mid]).tolist())[0]
+                sites[mid] = Site(mid, cand.keys(), cand)
+
+            for time in xrange(len(QList)):
+                qid = QList[time]
+                record['qid'] = qid
+                query = dict()
+                for mid in range(NumMach):
+                    query[mid] = matrix(data[qid])*WDict[mid]
+                for k in kList:
+                    record['k']=k
+                    ans, cost, level_rs, qcost =  MsWave(k, query, sites, pivot)
+                    naive = size(query[0])*NumMach + NumMach*k + k
+                    record['Cost'] = cost
+                    record['QCost'] = qcost
+                    record['NaiveCost']=naive
+                    record['LevelRs'] = '_'.join(str(x) for x in level_rs)
+                    record['RepeatTime']=time
+                    fout = open(OutFile,'ab')
+                    dw = csv.DictWriter(fout,headers,restval='NULL');
+                    dw.writerow(record)
+                    fout.close()
+                    for mid in range(NumMach):
+                        sites[mid].init_except_data();
+                print record
+                del query
+            del sites
+    del WDict
+fout.close()
